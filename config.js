8fb7f141618d60a4abdff0074988ed0b18e46bf3
halm <halm.paraline@gmail.com>
2024-11-16 11:05:28 +0700

    birth_day_mai

---
+++ config.js
@@ -0,0 +1,24 @@
+
+var config = {
+    // viết lời gửi gắm
+    texts: [
+        "Chúc Mừng Sinh Nhật",
+        "Mai",
+        "Nhé~~",
+    ],
+
+    imgs: {
+        "mai": "./imgs/mai_1.jpg",
+    },
+
+    desc: {
+        turn_on: "Bật Nên Nào",
+        play: "Music",
+        bannar_coming: "Bất Ngờ tiếp Nào 🤩",
+        balloons_flying: "Thiếu cái gì nhở 😁",
+        cake_fadein: "🎂 Đây !!!",
+        light_candle: "Thắp Nến Nên Đi Nà 🕯️🕯️🕯️",
+        wish_message: "WISH",
+        story: "Bất Ngờ Còn Ở Phía Sau",
+    }
+};
